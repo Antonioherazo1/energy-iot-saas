ae243db6220b64535851bce6e8f1a8acb25cdfa4
Antonio Herazo <antonioherazo7@hotmail.com>
2026-06-08 17:28:29 -0500

    feat: add S3 upload to backup script

--- scripts/backup_db.sh
+++ scripts/backup_db.sh
@@ -6,6 +6,7 @@ RETENTION_DAYS=30
 DB_USER="energy_iot"
 DB_NAME="energy_iot"
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+S3_BUCKET="${S3_BACKUP_BUCKET:-}"
 
 mkdir -p "$BACKUP_DIR"
 
@@ -16,3 +17,8 @@ sudo docker compose exec -T postgres pg_dump -U "$DB_USER" "$DB_NAME" | gzip > "
 find "$BACKUP_DIR" -name "${DB_NAME}_*.sql.gz" -mtime +$RETENTION_DAYS -delete
 
 echo "Backup created: ${BACKUP_DIR}/${DB_NAME}_${TIMESTAMP}.sql.gz"
+
+if [ -n "$S3_BUCKET" ] && command -v aws &>/dev/null; then
+  aws s3 cp "$BACKUP_DIR/${DB_NAME}_${TIMESTAMP}.sql.gz" "s3://${S3_BUCKET}/energy-iot/${DB_NAME}_${TIMESTAMP}.sql.gz"
+  echo "Backup uploaded to s3://${S3_BUCKET}/energy-iot/${DB_NAME}_${TIMESTAMP}.sql.gz"
+fi
