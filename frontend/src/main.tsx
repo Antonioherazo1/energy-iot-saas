59852d4bd16669d1fcd6874b68df022b51ea9410
Antonio Herazo <antonioherazo7@hotmail.com>
2026-07-15 02:03:45 -0500

    unregister service worker and clear caches on startup to force fresh content

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
+// Unregister any old service worker and clear caches from previous PWA build
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then((regs) => {
+    for (const reg of regs) reg.unregister();
+  });
+}
+if ("caches" in window) {
+  caches.keys().then((keys) => keys.forEach((key) => caches.delete(key)));
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
